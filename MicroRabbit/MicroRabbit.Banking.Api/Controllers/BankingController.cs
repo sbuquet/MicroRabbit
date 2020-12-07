c5b3e40ade41ef8f9d6ed0eeed186baee562cfd3
Sebastian Buquet <sdbuquet@hotmail.com>
2020-12-07 20:48:14 +0000

    Partiall completed Bankin Service

---
+++ MicroRabbit/MicroRabbit.Banking.Api/Controllers/BankingController.cs
@@ -0,0 +1,25 @@
+﻿using MicroRabbit.Banking.Application.Interfaces;
+using MicroRabbit.Banking.Domain.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MicroRabbit.Banking.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BankingController : ControllerBase
+    {
+        private readonly IAccountService _accountService;
+
+        public BankingController(IAccountService accountService)
+        {
+            _accountService = accountService;
+        }
+
+        // Get api/banking
+        [HttpGet]
+        public ActionResult<Account> Get()
+        {
+            return Ok(_accountService.GetAccounts());
+        }
+    }
+}
